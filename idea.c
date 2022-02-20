ec2ff9b697817191371afba43af8056210989895
Will Bettridge-Radford <william.bettridge.radford@gmail.com>
2022-02-20 17:14:01 +0000

    rename to cstring?

--- idea.c
+++ idea.c
@@ -8,14 +8,14 @@ struct fat_array {
     void *data;
 };
 
-struct array_char {
+struct cstring {
     size_t len;
     size_t cap;
     char *data;
 };
 
 void * alloc_array(size_t len, size_t cap, const char *data, size_t data_size, size_t elem_size);
-struct array_char * alloc_array_char(const char *s);
+struct cstring * alloc_cstring(const char *s);
 void die(const char const * msg);
 
 
@@ -24,7 +24,7 @@ main(int argc, char **argv)
 {
     /* Stack */
     char array_data_line1[10] = "a b c";
-    struct array_char line1 = {10, 10, array_data_line1};
+    struct cstring line1 = {10, 10, array_data_line1};
 
     puts("hi");
 
@@ -35,9 +35,11 @@ main(int argc, char **argv)
     printf("line1=\"%s\"\n", line1.data);
 
     /* Heap */
-    struct array_char *line2 = NULL;
-    line2 = alloc_array_char("hello2");
+    struct cstring *line2 = NULL;
+    line2 = alloc_cstring("hello2");
 
+    printf("line2=\"%s\"\n", line2->data);
+    line2->data[5] = '!';
     printf("line2=\"%s\"\n", line2->data);
 
     puts("bye");
@@ -45,12 +47,13 @@ main(int argc, char **argv)
 }
 
 
-struct array_char *
-alloc_array_char(const char *s)
+struct cstring *
+alloc_cstring(const char *s)
 {
     size_t slen = strlen(s);
+    size_t cap  = slen * 2;
     struct array_char *r =
-        alloc_array(slen, slen * 2, s, slen + 1, sizeof(char));
+        alloc_array(slen, cap, s, slen + 1, sizeof(char));
     return r;
 }
 
